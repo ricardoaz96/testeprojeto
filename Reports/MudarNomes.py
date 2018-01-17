bdab82a984856020dfa1a4cb6ce3e4d11fc5c55a
ricardoaz96 <34774198+ricardoaz96@users.noreply.github.com>
2018-01-17 01:09:08 +0000

    Add files via upload

---
+++ Reports/MudarNomes.py
@@ -0,0 +1,20 @@
+File = open("junit.xml","r")
+textodoficheiro = File.read()
+print(textodoficheiro)
+posicaodeclassname= textodoficheiro.find('classname') 
+print(posicaodeclassname)
+print(textodoficheiro.index('classname') )
+
+print(textodoficheiro[:posicaodeclassname+11])
+
+posicaodaultimaaspa = textodoficheiro.find("\"",posicaodeclassname+11)
+print(textodoficheiro.find("\"",posicaodeclassname+11))
+
+
+print(textodoficheiro[posicaodaultimaaspa:])
+
+
+textofinal=textodoficheiro[:posicaodeclassname+11] + "TROLOLOL" + textodoficheiro[posicaodaultimaaspa:]
+
+with open('junit.xml', 'w') as f:
+    f.write(textofinal)
